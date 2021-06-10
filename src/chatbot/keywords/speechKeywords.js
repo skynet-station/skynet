fa43ceda2827c0fb1e0bfce7734d7c6f066819c9
yh496 <yh496@cornell.edu>
2021-06-10 16:40:30 +0900

    improve stt response time

--- src/chatbot/keywords/speechKeywords.js
+++ src/chatbot/keywords/speechKeywords.js
@@ -2,15 +2,27 @@
 const speechToEventMap = {
     "주유": "gas",
     "주류": "gas",
+    "주야": "gas",
+    "중요": "gas",
+    "주연": "gas",
+    "주여": "gas",
+    "죽여": "gas",
     "기름": "gas",
     "출차": "leave",
+    "출처": "leave",
+    "가고 싶어요": "leave",
     "차": "leave",
     "날씨": "weather",
     "온도": "weather",
     "행사": "special",
     "예": "yes",
     "아니요": "no",
-    "춤": "sexy"
+    "춤": "sexy",
+    "네": "yes",
+    "그래": "yes",
+    "싫어요": "no",
+    "괜찮아요": "no",
+    "됐어요": "no"
 
 } 
 
--- src/stt.js
+++ src/stt.js
@@ -6,9 +6,10 @@ let speechClient = new speech.SpeechClient({
   credentials: require(process.env.GOOGLE_APPLICATION_CREDENTIALS)
 });
 
+console.log(Object.keys(speechToEventMap))
 const speechContextsElement = {
-  phrases: ['주유'],
-  boost: 100.0,
+  phrases: Object.entries(speechToEventMap),
+  boost: 20.0,
 };
 
 let options = {
@@ -21,7 +22,9 @@ let options = {
       speechContexts: [speechContextsElement],
 
   },
-  interimResults: false // If you want interim results, set this to true
+  interimResults: true, // If you want interim results, set this to true
+  single_utterance : true,//indicates whether this request should automatically end after speech is no longer detected. If set, Speech-to-Text will detect pauses, silence, or non-speech audio to determine when to end recognition.
+  silenceThreshold : 1000 //ms
 }
 
 let recognizeStream = null;
@@ -35,13 +38,21 @@ function startStreamRecognition (client) {
         stopStreamRecognition();
       })
       .on('data', (data) => {
-          const response  = processSpeechData(data)
-          console.log('what is my response', response, data.results[0].alternatives[0].transcript)
-          client.emit('response', response);
-
+        console.log(data.results[0].alternatives[0].transcript)
+        let detected = findKeyword(data.results[0].alternatives[0].transcript)
+        console.log('detecetd', detected)
+          if (detected.found) {
+              const response  = detected.eventName
+              console.log('immediately send to client as word detected', response, data.results[0].alternatives[0].transcript)
+              client.emit('response', response);
+            }
+  
           // if end of utterance, let's restart stream
           // this is a small hack. After 65 seconds of silence, the stream will still throw an error for speech length limit
-          if (data.results[0] && data.results[0].isFinal) {
+          else if (data.results[0] && data.results[0].isFinal) {
+              console.log('hi')
+              const response  = processSpeechData(data)
+              client.emit('response', response)
               stopStreamRecognition();
               startStreamRecognition(client);
               // console.log('restarted stream serverside');
@@ -68,6 +79,23 @@ module.exports = {
   startStreamRecognition, stopStreamRecognition, receiveAudioData
 }
 
+function findKeyword (speech) {
+  const keywords = speechToEventMap
+  let found = false
+  let eventName = null
+
+  for (const [key, value] of Object.entries(keywords)) {
+      const regex = new RegExp(key,"g")
+      found = speech.match(regex)
+      if (found) {
+        eventName = value
+        found = true
+        return {found,eventName}
+      }
+  }
+
+  return {found,eventName}
+}
 
 function processSpeechData (speechData) {
   const speech = speechData.results[0].alternatives[0].transcript
